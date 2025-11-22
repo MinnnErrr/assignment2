74cdde076ba34032dfb2715daabd3463e4d96fb8
MinnnErrr <lohminer22@gmail.com>
2025-11-22 16:21:42 +0800

    update

--- database/seeders/CustomerSeeders.php
+++ database/seeders/CustomerSeeders.php
@@ -2,11 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Str;
-// use Faker\Factory as Faker;
 use App\Models\Customer;
 
 class CustomerSeeders extends Seeder
